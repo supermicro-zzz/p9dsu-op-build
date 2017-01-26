b135153131e874e5636917782e892defe0a78274
jim <jim.yuan@supermicro.com>
2017-01-26 10:33:03 -0800

    point to xml from github/supermicro

--- openpower/package/p9dsu-xml/p9dsu.mk
+++ openpower/package/p9dsu-xml/p9dsu.mk
@@ -6,7 +6,7 @@
 
 P9DSU_XML_VERSION ?= 6f8bbfaf7ec9a7f888eaefcf77dc0f5a0d0b744c
 P9DSU_XML_SITE_METHOD = git
-P9DSU_XML_SITE = git@172.31.32.149:p9/p9dsu-xml.git
+P9DSU_XML_SITE = git@github.com:supermicro/p9dsu-xml.git
 
 P9DSU_XML_LICENSE = Apache-2.0
 P9DSU_XML_DEPENDENCIES = hostboot-install-images openpower-mrw-install-images common-p8-xml-install-images
